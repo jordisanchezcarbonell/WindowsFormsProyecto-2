b93bb50ac0fbf70e5ef9649f40b4c775cd665484
cep <visualstudio.cep@hotmail.com>
2019-04-04 17:51:15 +0200

    Per si acas

--- WindowsFormsApplication1/test/BD/HorarisInstalacions.cs
+++ WindowsFormsApplication1/test/BD/HorarisInstalacions.cs
@@ -8,13 +8,26 @@ namespace COMPLETE_FLAT_UI.BD
 {
     class HorarisInstalacions
     {
-        public static void InsertHorariInstalacio(int idInstalacio, int idDia, DateTime horaInici, DateTime horaFi)
+        public static void InsertHorarioInstalacion(int idInstalacion, int idDia, DateTime horaInicio, DateTime horaFin)
         {
             Instalacion_Horario horari = new Instalacion_Horario();
-            horari.id_instalacion = idInstalacio;
+            horari.id_instalacion = idInstalacion;
             horari.id_dia_semana = idDia;
-            horari.hora_inicio = horaInici;
-            horari.hora_final = horaFi;
+            horari.hora_inicio = horaInicio;
+            horari.hora_final = horaFin;
         }
+
+        public static String UpdateHorarioInstalacion(int id, DateTime horaInicio, DateTime horaFin)
+        {
+            String mensaje = "";
+            Instalacion_Horario instalacion = ORM.bd.Instalacion_Horario.Find(id);
+
+            instalacion.hora_inicio = horaInicio;
+            instalacion.hora_final = horaFin;
+            mensaje = ORM.SaveChanges();
+
+            return mensaje;
+        }
+
     }
 }
